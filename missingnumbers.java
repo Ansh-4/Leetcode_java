fd1d4c363abea724496d1bc348f5ee73d1c06df0
Ansh <anshdotcode@gmail.com>
2026-09-13 20:06:24 +0530

    missing numbers in an array

---
+++ missingnumbers.java
@@ -0,0 +1,10 @@
+class solution{
+    public int misssing(int[]nums){
+        int n = nums.length;
+        int sum =  n*(n+1)/2;
+        for(int num : nums){
+            sum -= num;
+        }
+        return sum;
+    }
+}
