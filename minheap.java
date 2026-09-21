7dfe0b7ac3833cc859854fd0cdfbe0c7e4e51595
Ansh <anshdotcode@gmail.com>
2026-09-21 21:08:23 +0530

    minHEAP

---
+++ minheap.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int value) {
+        stack.push(value);
+
+        if (minStack.isEmpty() || value <= minStack.peek()) {
+            minStack.push(value);
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) {
+            return;
+        }
+
+        int top = stack.pop();
+
+        if (top == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
