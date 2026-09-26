7ac9b82cecec34b976f4d1bc945be9628c47638f
Ansh <anshdotcode@gmail.com>
2026-09-26 20:20:38 +0530

    stocks

---
+++ buyandsellstocks.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int l =0, r=0;
+        int maxProfit = 0;
+        while(r<n){
+            if(prices[l]<prices[r]){
+                int profit = prices[r] - prices[l];
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                l=r;
+            }
+            r++;
+        }
+        return maxProfit;
+    }
+}
